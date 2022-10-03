cfbca5967e35dd8f9f2bf35d032381911b437d46
shiggityshaggs <shiggityshaggs@users.noreply.github.com>
2022-10-03 22:03:46 +0200

    x <> y

--- Maps/SlimeRancher2/main.js
+++ Maps/SlimeRancher2/main.js
@@ -64,8 +64,8 @@ function HoverHandler(evt) {
 
     let l = document.getElementById('list');
     l.innerHTML = outArr.join('');
-    l.style.top = `${evt.pixel[0] + hoverOffset}px`;
-    l.style.left = `${evt.pixel[1] + hoverOffset}px`;
+    l.style.left = `${evt.pixel[0] + hoverOffset}px`;
+    l.style.top = `${evt.pixel[1] + hoverOffset}px`;
 }
 
 window.addEventListener('load', () => {
