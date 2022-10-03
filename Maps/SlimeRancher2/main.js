6ea07b8a97b70c528321141fed5aefb6354791a1
shiggityshaggs <shiggityshaggs@users.noreply.github.com>
2022-10-03 09:35:20 +0200

    map layers -fc

--- Maps/SlimeRancher2/main.js
+++ Maps/SlimeRancher2/main.js
@@ -19,7 +19,7 @@ let map = new ol.Map({
     controls: ol.control.defaults({attribution: false}).extend([attribution]),
     target: 'map',
     view: view,
-    layers: [ World, POI_Group, featureCollection ],
+    layers: [ World, POI_Group ],
     loadend: console.log('map.loadend')
 });
 
